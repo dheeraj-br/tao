b95aea093f224c9533365c9ae3df5aa0f8ed78dc
dheeraj <dheeraj.br2@gmail.com>
2023-11-09 09:02:09 +0530

    update router

--- src/components/getStarted/GetStarted.js
+++ src/components/getStarted/GetStarted.js
@@ -1,19 +1,19 @@
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function GetStarted() {
     return (
         <section className="py-5 bg-custom">
             <div className="container">
                 <div className="row">
-                    <div className="h-50 d-inline-block"><br /><br /><br /><br /><br /><br /><br /><br /></div>
-                    <div className="text-start mt-auto">
-                        {/* <Link to="/access">
-                            <button type="button" className="col-4 mx-auto btn bg-white fw-bold text-primary">Get Started</button>
-                        </Link> */}
+                    <Link to="/access" className="col-lg-6 text-start mt-auto">
+                        <button type="button" className="col-4 mx-auto btn bg-white fw-bold text-primary">Get Started</button>
+                    </Link>
+                    <div className="col-lg-6 text-end">
+                        <img src="/images/img1.png" alt="logo" width="350" />
                     </div>
                 </div>
             </div>
-        </section>
+        </section >
     );
 }
 
